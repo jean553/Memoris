5e5767c01830bb7a1038c0d6ed5ea246a3be3b72
Jean LELIEVRE <jean@bst.com>
2015-04-07 00:23:12 +0800

    create the main.cpp file

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+/**
+ * @file main.cpp
+ * @author Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
+ *
+ * Starting point of the program
+ */
+
+#include <SFML/Graphics.hpp>
+
+/**
+ * Main function of the program, load the window
+ * and start all the program components
+ *
+ * @return EXIT_SUCCESS when the program correctly finish
+ */
+int main() 
+{
+    
+    // Create a window fullscreen and
+    // force the resolution to 1600x900
+    sf::RenderWindow *window = new sf::RenderWindow(
+        sf::VideoMode(1600, 900, 32), 
+        "SFML",
+        sf::Style::Fullscreen
+    );
+
+    return EXIT_SUCCESS;
+}
