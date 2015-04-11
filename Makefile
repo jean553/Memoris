8865dca403acaaa8ca2d362217cc64c2f541b8c0
Jean LELIEVRE <jean@bst.com>
2015-04-11 19:43:46 +0800

    close #12 integrate the screen factory class

--- Makefile
+++ Makefile
@@ -2,4 +2,5 @@ Memoris: src/main.cpp
 	g++ -c src/main.cpp -o bin/main.o
 	g++ -c src/controllers/GameController.cpp -o bin/GameController.o
 	g++ -c src/controllers/MainMenuController.cpp -o bin/MainMenuController.o
+	g++ -c src/factories/ScreenFactory.cpp -o bin/ScreenFactory.o
 	g++ bin/main.o bin/GameController.o bin/MainMenuController.o -o bin/Memoris -lsfml-graphics -lsfml-window -lsfml-system
---
+++ src/factories/ScreenFactory.hpp
@@ -0,0 +1,24 @@
+/**
+ * Factory which generates the screens.
+ *
+ * @file ScreenFactory.hpp
+ * @brief factory to generate the game screens
+ * @package factories
+ * @author Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
+ */
+
+#ifndef DEF_SCREEN_FACTORY
+#define DEF_SCREEN_FACTORY
+
+namespace factories
+{
+    class ScreenFactory
+    {
+        public:
+
+            ScreenFactory();
+            ~ScreenFactory();
+    };
+}
+
+#endif
