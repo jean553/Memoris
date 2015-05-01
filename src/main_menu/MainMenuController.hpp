53288f2ca7546ffe4e0a362ad7581bf0fa3016e8
jean <jean@bst>
2015-05-01 18:13:27 +0800

    close #37 integrate the music of the main menu

--- src/main_menu/MainMenuController.hpp
+++ src/main_menu/MainMenuController.hpp
@@ -11,10 +11,13 @@
 #define DEF_MAIN_MENU_CONTROLLER
 
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 
 #include "../utils/Controller.hpp"
 
 #define TITLE_FONT_SIZE 80
+#define PATH_FONT_TITLE "res/fonts/designer_block.ttf"
+#define PATH_MUSIC_MAIN_MENU "res/musics/dj-xtrm-killer-symphonyX-outro.ogg"
 
 namespace controllers
 {
@@ -37,6 +40,8 @@ namespace controllers
             sf::Font fontTitle;
 
             sf::Text title;
+
+            sf::Music musicMainMenu;
     };
 }
 
