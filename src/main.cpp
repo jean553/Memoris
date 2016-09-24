7aab61207117831420a7001c8a33ba836b290050
jean <jean@bst>
2016-09-24 16:20:21 +0800

    close refactor main

--- src/main.cpp
+++ src/main.cpp
@@ -36,119 +36,61 @@ using namespace memoris;
  */
 int main()
 {
-    /* the current controller id variable contains the id of the currently
-       rendered controller; when the program starts, the first controller
-       to display is the main menu controller. */
-    unsigned short currentControllerId {controllers::MAIN_MENU_CONTROLLER_ID};
+    unsigned short currentControllerId {controllers::MAIN_MENU_CONTROLLER_ID},
+        nextControllerId {0};
 
-    /* the current music path string contains the file system path of the
-       music currently played by the current controller; when the program
-       starts, the played music is the menu music */
-    std::string currentMusicPath =
-        musics::getMusicPathById(currentControllerId);
+    std::string currentMusicPath = 
+        musics::getMusicPathById(currentControllerId),
+        nextMusicPath;
 
-    /* create the unique context object of the program */
     utils::Context context;
-
-    /* load and play the main menu music when the program starts */
     context.loadMusicFile(currentMusicPath);
 
-    /* the next controller id contains the id of the new controller to
-       initialize. After generation of a controller, the current controller id
-       is equal to the next controller id, the next controller id is equal
-       to 0. When a controller calls a new controller, the next controller id
-       variable is updated. If the next controller id and the current
-       controller id are different, the controller with the nextControllerId
-       value is generated.*/
-    unsigned short nextControllerId {0};
-
-    /* the next music path string contains the path of the next music to
-       load and play during the next controller generation. Sometimes, from
-       one controller to another, the currentMusicPath and the nxtMusicPath are
-       the sames, there is no interupt and modification of the playing music;
-       if they are different during the generation of a new controller, a new
-       music will be loaded using the nextMusicPath */
-    std::string nextMusicPath;
-
-    /* first loop, calls the render loop and create each controller
-       when necessary */
     do
     {
-        /* create a unique pointer to the current controller, pointing to the
-           controller to render; we use a pointer as there are different
-           children classes of controllers, all children of the Controller
-           class; we use an unique pointer, clearly appropriate for factories
-           functions; as it is a smart pointer, we do not need to delete it
-           manually after each loop iteration; we use auto here because we
-           will always return an unique pointer to a controller */
-
-        /* NOTE: we use 'auto' for automatic deduction here. The function
-           getControllerById() returns a pointer to a Controller object. The
-           deduction is equivalent to :
-           template<typename T> void f(T* param); f(getControllerById());
-           In that case, 'param' type is Controller* and T is Controller,
-           so the deducted variable type is a pointer to a Controller object */
         auto pCurrentController =
             controllers::getControllerById(
                 context,
                 currentControllerId
-            );
+            ); // auto -> std::unique_ptr<controllers::Controller>
 
-        /* main program loop: loads, renders and modifies controllers;
-           the loop stops when the SFML window is closed */
         do
         {
-            /* continually clear the whole SFML window content */
             /* NOTE: this instruction generates memory leaks; this error comes
                from OpenGL libraries; we do not handle this error; the leak is
                deleted if we delete this line */
             context.getSfmlWindow().clear();
 
-            /* continually render the current controller scene */
             nextControllerId = pCurrentController->render(context);
 
-            /* continually display the loaded content */
             context.getSfmlWindow().display();
         }
         while (!nextControllerId);
 
-        /* check if the program is supposed to be terminated */
-        if (currentControllerId != controllers::EXIT)
+        if (currentControllerId == controllers::EXIT)
         {
-            /* get the path of the new music to load according to the next
-               controller to load; sometimes, the music path is the same as
-               the current controller music path */
-            nextMusicPath =
-                musics::getMusicPathById(nextControllerId);
-
-            /* check if the current music path and the next music path are
-               different; if they are different, a new music has to be
-               loaded */
-            if(currentMusicPath != nextMusicPath)
-            {
-                /* if the current music path and the next music path are
-                   different, load and play the new music according to the
-                   next music path */
-                context.loadMusicFile(nextMusicPath);
-                currentMusicPath = nextMusicPath;
-            }
-
-            currentControllerId = nextControllerId;
-
-            /* if the program is not supposed to be terminated, the screen
-               content is updated: a common sound is played during the
-               visual switch */
-            context.getSoundsManager().playScreenTransitionSound();
-
-            /* restart the clock */
-            context.restartClock();
+            continue;
         }
+
+        nextMusicPath =
+            musics::getMusicPathById(nextControllerId);
+
+        if(currentMusicPath != nextMusicPath)
+        {
+            context.loadMusicFile(nextMusicPath);
+            currentMusicPath = nextMusicPath;
+        }
+
+        currentControllerId = nextControllerId;
+
+        context.getSoundsManager().playScreenTransitionSound();
+
+        context.restartClock();
     }
     while (currentControllerId != controllers::EXIT);
 
     context.stopMusic();
 
-    /* close the SFML window */
     context.getSfmlWindow().close();
 
     return EXIT_SUCCESS;
