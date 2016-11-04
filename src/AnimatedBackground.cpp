5fda9197a6ef52bca81e5b27b88a9354731bc676
jean <jean@bst>
2016-11-04 22:18:21 +0800

    close add cells into the animated background

--- src/AnimatedBackground.cpp
+++ src/AnimatedBackground.cpp
@@ -104,7 +104,14 @@ void AnimatedBackground::initializeCells(utils::Context& context) &
         cells::LESS_TIME_CELL,
         cells::WALL_CELL,
         cells::STAIRS_UP_CELL,
-        cells::STAIRS_DOWN_CELL
+        cells::STAIRS_DOWN_CELL,
+        cells::HORIZONTAL_MIRROR_CELL,
+        cells::VERTICAL_MIRROR_CELL,
+        cells::DIAGONAL_CELL,
+        cells::LEFT_ROTATION_CELL,
+        cells::RIGHT_ROTATION_CELL,
+        cells::ELEVATOR_UP_CELL,
+        cells::ELEVATOR_DOWN_CELL
     };
 
     unsigned short currentLine {0}, currentColumn {0};
@@ -117,34 +124,22 @@ void AnimatedBackground::initializeCells(utils::Context& context) &
 
     srand(time(NULL));
 
-    for (unsigned int i = 0; i < 575; i++)
+    for (
+        unsigned short index = 0;
+        index < BACKGROUND_CELLS_AMOUNT;
+        index++
+    )
     {
-        /* select a cell randomly; the maximum value of the generated number
-           is 17 even if there are only 11 different cells to generate; if the
-           random number is more than 11, the 11 value will be used anyway, so
-           there are more chances to get the value 11; this is because it is
-           better to have manu "null cells" ( black cells ), to generate a
-           beautiful background */
-        randomNumber = rand() % 17;
+        randomNumber = rand() % MAXIMUM_RANDOM_NUMBER;
 
         currentLine++;
 
-        if (currentLine == 18)
+        if (currentLine == CELLS_PER_COLUMN)
         {
             currentLine = 0;
-
             currentColumn++;
         }
 
-        /* the random number can be up to 16, there is no so much available
-           cells type in the cells library; if the number is more than 16,
-           the position is incremented for the next cell but the cell at the
-           current position is not created; this is used to show some blank
-           black spaces into the background; this is optimized because we
-           do not create any useless cell (cell that is not displayed); we
-           get the total size of the cells lib in bytes, divided by the size
-           of one char type in bytes; like that, we can get the number of items
-           into the cells lib automatically */
         if(randomNumber >= (sizeof(cellsLib) / sizeof(char)))
         {
             continue;
