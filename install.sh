47d97f13bb04f2eade78bea715da3931a7a59cab
alexis-turpin <106575@supinfo.com>
2016-11-17 07:38:26 -0600

    close #699 create one liner to compile/run the game
    create a curl pipe bash one liner to compile and run the game

---
+++ install.sh
@@ -0,0 +1,11 @@
+add-apt-repository ppa:ubuntu-toolchain-r/test -y
+apt-get update
+apt-get install g++-4.9 libsfml-dev cmake clang-3.5 git -y
+add-apt-repository --remove ppa:ubuntu-toolchain-r/test -y
+
+git clone https://github.com/jean553/Memoris
+cd ./Memoris/
+mkdir build && cd build/
+cmake ..
+make
+cd ./bin/
