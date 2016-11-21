a6dfe5d96882716f66c9d7ca8259dd4c6c66fbe3
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2016-11-21 22:44:43 +0800

    Fix path error when change directory

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ apt-get install -y g++-4.9 libsfml-dev cmake clang-3.5 git
 add-apt-repository -y --remove ppa:ubuntu-toolchain-r/test
 
 git clone https://github.com/jean553/Memoris
-cd ./Memoris/
+cd Memoris/
 mkdir build && cd build/
 cmake ..
 make
