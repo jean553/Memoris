da64087e979e103063efa7f0316fe232acc1cfb5
jean <jean@bst>
2017-03-16 22:34:02 +0800

    update copyright years in includes

--- includes/dimensions.hpp
+++ includes/dimensions.hpp
@@ -1,6 +1,6 @@
 /**
  * Memoris
- * Copyright (C) 2016  Jean LELIEVRE
+ * Copyright (C) 2017  Jean LELIEVRE
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
