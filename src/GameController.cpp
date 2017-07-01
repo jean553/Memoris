9bae7ec1636fa0bfde6d3662ed4b0482cb1b9331
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-07-01 21:34:55 +0200

    use an enumeration to indicate the stairs animation direction

--- src/GameController.cpp
+++ src/GameController.cpp
@@ -272,7 +272,7 @@ const ControllerId& GameController::render() const &
                         context,
                         level,
                         floor,
-                        1
+                        animations::StairsAnimation::FloorMoveDirection::Up
                     );
 
                     impl->movePlayerToNextFloor = true;
@@ -714,7 +714,7 @@ std::unique_ptr<animations::LevelAnimation> GameController::getAnimationByCell(
                    context,
                    level,
                    floor,
-                   1
+                   animations::StairsAnimation::FloorMoveDirection::Up
                );
     }
     case cells::ELEVATOR_DOWN_CELL:
@@ -724,7 +724,7 @@ std::unique_ptr<animations::LevelAnimation> GameController::getAnimationByCell(
                    context,
                    level,
                    floor,
-                   -1
+                   animations::StairsAnimation::FloorMoveDirection::Down
                );
     }
     case cells::DIAGONAL_CELL:
