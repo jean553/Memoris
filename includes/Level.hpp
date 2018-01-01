3d4cae155801591b077a39854d0300282d986520
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2018-01-01 21:13:11 +0100

    add details into the documentation of the function that set the cell type, as this function now handles the creation of the stairs cells (down and up) if the current inserted cell is a stair cell

--- includes/Level.hpp
+++ includes/Level.hpp
@@ -299,6 +299,10 @@ public:
      * replaces it with the given type; returns a boolean that
      * indicates if the level has been modified or not
      *
+     * NOTE: this method also automatically inserts a stairs up
+     * or stairs down cell if the currently inserted cell is a stair
+     * and there is a floor available above or below the current floor
+     *
      * @param floor the current level floor displayed
      * @param type the type to apply on the 'mouse hover' cell
      *
