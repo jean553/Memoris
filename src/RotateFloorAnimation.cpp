325b1e2480a33569fa5e1ca0584e59a8a5234838
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2018-01-04 23:58:08 +0100

    refactor the rotate cells animation for one quarter

--- src/RotateFloorAnimation.cpp
+++ src/RotateFloorAnimation.cpp
@@ -137,172 +137,31 @@ void RotateFloorAnimation::playNextAnimationStep() const &
  */
 void RotateFloorAnimation::rotateCells() const &
 {
-    /* TODO: #1194 numbers literals should be constant expressions */
-
-    std::vector<std::vector<entities::Cell>> horizontalLines;
-
-    unsigned short playerColumn {0};
-    unsigned short playerIndex {0};
-
     const auto& level = getLevel();
-    const auto& context = getContext();
-    const auto& floor = getFloor();
+    const auto& cells = level->getCells(); 
 
-    unsigned short destination = floor * 256 + 255;
-
-    if(impl->direction == -1)
+    constexpr unsigned short CELLS_PER_SIDE = dimensions::CELLS_PER_FLOOR / 2;
+    for (
+        unsigned short index = 0;
+        index < CELLS_PER_SIDE;
+        index += 1
+    )
     {
-        destination = floor * 256 + 240;
-
-        for (
-            unsigned short lines = 0;
-            lines < 16;
-            lines++
-        )
+        if (index % dimensions::CELLS_PER_LINE >= 8)
         {
-            std::vector<entities::Cell> line;
-
-            for (
-                unsigned short index = 0;
-                index < 16;
-                index++
-            )
-            {
-                entities::Cell cell(
-                    context,
-                    0.f,
-                    0.f,
-                    level->getCells()[lines * 16 + index]->getType()
-                );
-
-                cell.setIsVisible(
-                    level->getCells()[lines * 16 + index]->isVisible()
-                );
-
-                line.push_back((*level->getCells()[lines * 16 + index]));
-
-                if (lines * 16 + index == level->getPlayerCellIndex())
-                {
-                    playerColumn = lines;
-                    playerIndex = index;
-                }
-            }
-
-            horizontalLines.push_back(line);
+            continue;
         }
 
-        for (unsigned short column = 0; column < 16; column++)
-        {
-            for (unsigned short index = 0; index < 16; index++)
-            {
-                level->getCells()[destination]->setType(
-                    horizontalLines[column][index].getType()
-                );
+        const auto type = cells[index]->getType();
+        const std::pair<short, short> coordinates =
+            getCoordinatesFromIndex(index);
 
-                if (horizontalLines[column][index].isVisible())
-                {
-                    level->getCells()[destination]->show(context);
-                }
-                else
-                {
-                    level->getCells()[destination]->hide(context);
-                }
+        /* rotate (x,y) around the origin (0, 0) results into (-y, x) */
+        const short x = coordinates.second * -1;
+        const short y = coordinates.first;
 
-                if (column == playerColumn && index == playerIndex)
-                {
-                    level->setPlayerCellIndex(destination);
-                }
-
-                if (destination >= 16)
-                {
-                    destination -= 16;
-                }
-            }
-
-            if (destination != 15)
-            {
-                destination += 241;
-            }
-        }
-    }
-    else
-    {
-        destination = floor * 256 + 255;
-
-        for (
-            unsigned short lines = 0;
-            lines < 16;
-            lines++
-        )
-        {
-            unsigned short offset = 0;
-
-            std::vector<entities::Cell> line;
-
-            for (
-                short index = 15;
-                index >= 0;
-                index--
-            )
-            {
-                entities::Cell cell(
-                    context,
-                    0.f,
-                    0.f,
-                    level->getCells()[lines * 16 + index]->getType()
-                );
-
-                cell.setIsVisible(
-                    level->getCells()[lines * 16 + index]->isVisible()
-                );
-
-                line.push_back((*level->getCells()[lines * 16 + index]));
-
-                if (lines * 16 + index == level->getPlayerCellIndex())
-                {
-                    playerColumn = lines;
-                    playerIndex = offset;
-                }
-
-                offset++;
-            }
-
-            horizontalLines.push_back(line);
-        }
-
-        for (short column = 0; column < 16; column++)
-        {
-            for (unsigned short index = 0; index < 16; index++)
-            {
-                level->getCells()[destination]->setType(
-                    horizontalLines[column][index].getType()
-                );
-
-                if (horizontalLines[column][index].isVisible())
-                {
-                    level->getCells()[destination]->show(context);
-                }
-                else
-                {
-                    level->getCells()[destination]->hide(context);
-                }
-
-                if (column == playerColumn && index == playerIndex)
-                {
-                    level->setPlayerCellIndex(destination);
-                }
-
-                if (destination >= 16)
-                {
-                    destination -= 16;
-                }
-            }
-
-            if (destination != 0)
-            {
-                destination += 239;
-            }
-        }
+        const auto destinationIndex = getIndexFromCoordinates(x, y);
+        cells[destinationIndex]->setType(type);
     }
 }
 
